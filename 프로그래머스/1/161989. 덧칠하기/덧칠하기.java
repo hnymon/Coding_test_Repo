4008b651704fc040c6edf567d623baf3821bd796
BeomHeon Yoon <151509541+hnymon@users.noreply.github.com>
2024-03-29 14:40:17 +0900

    [level 1] Title: 덧칠하기, Time: 4.58 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -4,16 +4,14 @@ class Solution {
         // 페인트 배열
         // 다시 칠 : 1
         int[] arr = new int[n];
-        for(int i=0; i<section.length; i++){
-            arr[section[i]-1] = 1;
+        for (int i = 0; i < section.length; i++) {
+            arr[section[i] - 1] = 1;
         }
-        for(int i=0; i<n; i++){
-            if(arr[i] == 1) {
-                // 페인트칠을 해야 하는 구간이 시작될 때마다 횟수 증가
-                answer++; 
-                for(int j=i; j<Math.min(i+m, n); j++) {
-                    // 구간을 다시 칠하고 페인트 배열을 0으로 초기화
-                    arr[j] = 0; 
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 1) {
+                answer++;
+                for (int j = i; j >= Math.max(0, i - m + 1); j--) {
+                    arr[j] = 0;
                 }
             }
         }
