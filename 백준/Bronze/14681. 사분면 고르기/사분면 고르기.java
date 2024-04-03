0e3a4198b157c4171c9bd110936c93e1379ddfd0
BeomHeon Yoon <151509541+hnymon@users.noreply.github.com>
2024-04-03 19:06:21 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n1 = Integer.parseInt(br.readLine());
+		int n2 = Integer.parseInt(br.readLine());
+		if(n1>0 && n2>0) System.out.println(1);
+		else if(n1<0 && n2>0) System.out.println(2);
+		else if(n1<0 && n2<0) System.out.println(3);
+		else System.out.println(4);
+		br.close();
+	}
+}
