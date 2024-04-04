b66fceb40c07ce1d43bd03f32b537c615ebd0a18
BeomHeon Yoon <151509541+hnymon@users.noreply.github.com>
2024-04-04 15:22:05 +0900

    [Bronze V] Title: A+B - 4, Time: 144 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String line = br.readLine();
+			if (line == null || line.equals("")) {
+				break;
+			}
+			StringTokenizer st = new StringTokenizer(line);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.write(a + b + "\n");
+		}
+		bw.close();
+		br.close();
+	}
+}
