ec133f033c5dbb846d109e5e7e418477cf2e9ba1
BeomHeon Yoon <151509541+hnymon@users.noreply.github.com>
2024-04-08 17:48:43 +0900

    [Bronze V] Title: 문자열, Time: 140 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for(int i=0; i<T; i++) {
+			String s = br.readLine();
+			bw.write(s.charAt(0) +""+ s.charAt(s.length()-1) +"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
